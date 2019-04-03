aabc7104d545e231094f52710d588da38792e073
seanbanford <seanbanford@gmail.com>
2019-04-03 15:07:39 +0100

    test helpers complete

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter player names' do
   scenario 'Players can enter names and click submit' do
-    visit('/')
-    fill_in :player_1_name, with: 'Ted'
-    fill_in :player_2_name, with: 'Fred'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Ted vs. Fred'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'See hit points of player 2' do
-    visit('/')
-    fill_in :player_1_name, with: 'Ted'
-    fill_in :player_2_name, with: 'Fred'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Fred: 50HP'
   end
 end
